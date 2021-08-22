f6baa5e7b23c3656d33472eded88850b53c10d96
APiscoci <piscoci.anca@gmail.com>
2021-08-22 15:47:09 +0100

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>App Header</header>
+      <div>Products</div>
+      <div>Shopping Basket</div>
+    </div>
+  );
+}
+
+export default App;
