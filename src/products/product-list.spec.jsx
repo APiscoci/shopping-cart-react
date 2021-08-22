8e22824c45a8e84b04e2fc77c27c2ad7cdc34c1e
APiscoci <piscoci.anca@gmail.com>
2021-08-22 17:06:58 +0100

    test: add tests for product list

---
+++ src/products/product-list.spec.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { render } from "@testing-library/react";
+import ProductList from "./product-list";
+import data from "../data/data";
+
+describe("Product List Component", () => {
+  const { products } = data;
+  it("should render the header without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<ProductList />, div);
+  });
+
+  it("should render the products correctly", () => {
+    const { getByTestId } = render(<ProductList products={products} />);
+    products.forEach((product) => {
+      expect(
+        getByTestId(`product-${product.stockKeepingUnit}`).textContent
+      ).toBe(product.stockKeepingUnit);
+    });
+  });
+
+  it("should render the add to basket buttons", () => {
+    const { getByTestId } = render(<ProductList products={products} />);
+    products.forEach((product) => {
+      expect(
+        getByTestId(`add-to-basket-button-product-${product.stockKeepingUnit}`)
+          .textContent
+      ).toBeTruthy();
+    });
+  });
+});
