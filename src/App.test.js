17c7d781f4cd3af5517004c9a9f02276271bc59f
APiscoci <piscoci.anca@gmail.com>
2021-08-22 16:20:05 +0100

    feat: add initial test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,60 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { render, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App tests", () => {
+  it("should render the page without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+  });
+
+  it("should be able to add item to the basket", () => {
+    const { getByTestId } = render(<App />);
+
+    // When
+    fireEvent.click(getByTestId("add-to-basket-button-product-A"));
+
+    // Then
+    expect(getByTestId("basket-product-stock-keeping-unit-A")).toBeTruthy();
+  });
+
+  it("should show the total cost of the basket", () => {
+    const { getByTestId } = render(<App />);
+
+    // When
+    fireEvent.click(getByTestId("add-to-basket-button-product-A"));
+
+    // Then
+    expect(getByTestId("shopping-basket-total-price").textContent).toBe(
+      "Total: £10.00"
+    );
+  });
+
+  it("should apply promotion to every multiple of 3", () => {
+    const { getByTestId } = render(<App />);
+
+    // When
+    fireEvent.click(getByTestId("add-to-basket-button-product-B"));
+    fireEvent.click(getByTestId("add-to-basket-button-product-B"));
+    fireEvent.click(getByTestId("add-to-basket-button-product-B"));
+
+    // Then
+    expect(getByTestId("shopping-basket-total-price").textContent).toBe(
+      "Total: £40.00"
+    );
+  });
+
+  it("should apply promotion of 25% off to evey multiple of 2", () => {
+    const { getByTestId } = render(<App />);
+
+    // When
+    fireEvent.click(getByTestId("add-to-basket-button-product-D"));
+    fireEvent.click(getByTestId("add-to-basket-button-product-D"));
+
+    //Then
+    expect(getByTestId("shopping-basket-total-price").textContent).toBe(
+      "Total: £82.50"
+    );
+  });
 });
